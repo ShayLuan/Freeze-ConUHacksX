c384a0a68c736f26acde909d9dba846926ded0d8
faydum516 <77711700+faydum516@users.noreply.github.com>
2026-01-24 15:35:58 -0500

    modified db.js

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const pool = require("../config/db");
+const pool = require("../db");
 const sendEmail = require("../utils/email");
 
 // REGISTER
